8c4cadfa138632fbe9776bdaec2aa5a23e733bf1
Abdulmajid <ermatovabdumajid23@gmail.com>
2024-07-12 07:42:38 +0500

    Initial commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import { appleImg } from "../utils";
+
+const NavBar = () => {
+  return (
+    <header>
+      <nav>
+        <img src={appleImg} alt="logo-apple" width={14} height={18} />
+
+        <div>
+          
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
