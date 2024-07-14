73e5fdcbf0a3e5cc980ad68cb223e34603eb2838
Abdulmajid <ermatovabdumajid23@gmail.com>
2024-07-14 10:07:25 +0500

    HowItWorks section implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Features from "./components/Features";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
+import HowItWorks from "./components/HowItWorks";
 import Modal from "./components/Modal";
 import NavBar from "./components/NavBar";
 
@@ -14,6 +15,7 @@ const App = () => {
       <Highlights />
       <Modal />
       <Features />
+      <HowItWorks />
     </main>
   );
 };
